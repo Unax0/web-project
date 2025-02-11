5eb70b8a29091f0ebbac78b5eb5ede52fd5a600f
Vyacheslav <34347@iitu.edu.kz>
2025-02-12 01:20:16 +0500

    step 13 Preparing for Todo List Project status filter

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; // важно: 'react-dom/client'
+import ReactDOM from "react-dom/client";
 import AppHeader from "./components/app-header";
 import SearchPanel from "./components/search-panel";
 import TodoList from "./components/todo-list";
 
 const App = () => {
-
     const todoData = [
-        { label: 'Drink Coffee', important: false,  id: 1},
-        { label: 'Implement React Application', important: true, id: 2 },
-        { label: 'Make notes from your stud', important: false, id: 3 },
-        { label: 'Do not drink alchohol', important: false, id: 4 },
+        { label: "Drink Coffee", important: false, id: 1 },
+        { label: "Implement React Application", important: true, id: 2 },
+        { label: "Make notes from your stud", important: false, id: 3 },
+        { label: "Do not drink alcohol", important: false, id: 4 },
     ];
 
     return (
         <div>
             <AppHeader />
             <SearchPanel />
-            <TodoList todos =  {todoData} />
+            <TodoList todos={todoData} />
         </div>
     );
-}
+};
+
+// Новый способ рендера в React 18+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
