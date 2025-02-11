d5e1b4893064b2469d18407ce695b8fdfc9173da
Vyacheslav <34347@iitu.edu.kz>
2025-02-12 01:24:55 +0500

    step 14 Editing Project structure

--- src/index.js
+++ src/index.js
@@ -1,26 +1,28 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom';
+
 import AppHeader from "./components/app-header";
 import SearchPanel from "./components/search-panel";
 import TodoList from "./components/todo-list";
+import ItemStatusFilter from "./components/item-status-filter";
 
 const App = () => {
+
     const todoData = [
-        { label: "Drink Coffee", important: false, id: 1 },
-        { label: "Implement React Application", important: true, id: 2 },
-        { label: "Make notes from your stud", important: false, id: 3 },
-        { label: "Do not drink alcohol", important: false, id: 4 },
+        { label: 'Drink Coffee', important: false,  id: 1},
+        { label: 'Implement React Application', important: true, id: 2 },
+        { label: 'Make notes from your stud', important: false, id: 3 },
+        { label: 'Do not drink alchohol', important: false, id: 4 },
     ];
 
     return (
         <div>
             <AppHeader />
             <SearchPanel />
-            <TodoList todos={todoData} />
+            <TodoList todos =  {todoData} />
+            <ItemStatusFilter />
         </div>
     );
-};
-
-// Новый способ рендера в React 18+
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
