39ef441dbb60e198bab5d9dd890d1b119022c853
Vyacheslav <34347@iitu.edu.kz>
2025-02-19 11:17:09 +0500

    step 35 - Filter component implementation

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,7 +17,8 @@ export  default  class  App extends Component {
             this.createTodoItem('Make Awesome App'),
             this.createTodoItem('Have a lunch')
         ],
-        term: ''
+        term: '',
+        filter: 'all'
     };
     createTodoItem(label) {
         return {
@@ -25,6 +26,23 @@ export  default  class  App extends Component {
             important: false,
             done: false,
             id: this.maxId++
+        }
+    }
+
+    onFilterChange = (filter) => {
+        this.setState({filter});
+    };
+
+    filter(items, filter) {
+        switch (filter) {
+            case 'all':
+                return items;
+            case 'active':
+                return items.filter((item) => !item.done);
+            case 'done':
+                return items.filter((item) => item.done);
+            default:
+                return items;
         }
     }
 
@@ -98,8 +116,8 @@ export  default  class  App extends Component {
     }
 
     render() {
-        const {todoData, term} = this.state;
-        const visibleItems = this.search(todoData, term);
+        const {todoData, term, filter} = this.state;
+        const visibleItems = this.filter(this.search(todoData, term), filter);
         const doneCount = todoData.filter((element) => element.done).length;
         const todoCount = todoData.length - doneCount;
 
@@ -108,7 +126,9 @@ export  default  class  App extends Component {
                 <AppHeader toDo={todoCount} done={doneCount}/>
                 <div className="top-panel d-flex">
                     <SearchPanel onSearchChange = {this.onSearchChange}/>
-                    <ItemStatusFilter/>
+                    <ItemStatusFilter
+                        filter = {filter}
+                        onFilterChange = {this.onFilterChange}/>
                 </div>
 
                 <TodoList todos={visibleItems}
